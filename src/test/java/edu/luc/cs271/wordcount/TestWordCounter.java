d9b062bb48a9998576812ed278a8e61ed2754acd
Ariane Quenum <qariane@luc.edu>
2017-11-07 12:13:30 +0000

    Descendingbycount complete

--- src/test/java/edu/luc/cs271/wordcount/TestWordCounter.java
+++ src/test/java/edu/luc/cs271/wordcount/TestWordCounter.java
@@ -11,22 +11,22 @@ public class TestWordCounter {
   // TODO complete this test class
 
   // TODO declare a reference to the SUT (system under test), i.e., WordCounter
-
+   
   @Before
   public void setUp() {
     // TODO create the SUT instance
-  }
-
+    
   @After
   public void tearDown() {
     // TODO set the SUT instance to null
+  
   }
 
   @Test
   public void testGetCountEmpty() {
 
     // TODO verify that the SUT initially returns an empty map
-    fail();
+    
 
   }
 
@@ -36,7 +36,4 @@ public class TestWordCounter {
     // TODO run the SUT on a specific String iterator with some repeated words,
     // then use assertions to verify the correct counts
     // do this for at least two words in the iterator and two not in the iterator
-    fail();
-
-  }
-}
+   
