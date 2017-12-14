a4484039563f3c95946a9a56b80c60e999d2222a
Ariane Quenum <qariane@luc.edu>
2017-12-14 12:57:49 +0000

    build successfully

---
+++ src/main.java
@@ -0,0 +1,30 @@
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Main {
+  static JFrame frame;
+  static Puzzle puzzle;
+
+  public static void main(String[] args) {
+    frame = new JFrame("Puzzlegame");
+    frame.setSize(327, 327);
+
+    puzzle = new Puzzle(new ImageIcon(Main.class.getResource("/Image.png")).getImage());
+
+    frame.add(puzzle);
+
+    frame.setLocationRelativeTo(null);
+
+    frame.setResizable(false);
+    frame.setVisible(true);
+
+    frame.addMouseListener(
+        new MouseAdapter() {
+          public void mousePressed(MouseEvent e) {
+            if (!puzzle.started) puzzle.start();
+            else if (puzzle.mixing) puzzle.mixing = false;
+            else puzzle.onClick(e);
+          }
+        });
+  }
+}
